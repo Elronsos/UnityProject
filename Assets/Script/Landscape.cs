697b73ef39e6fe06ca692860c2c162d4bdc4ddde
Elronsos <maxengelen@hotmail.de>
2017-10-05 19:40:01 +0200

    Add files via upload

--- Assets/Script/Landscape.cs
+++ Assets/Script/Landscape.cs
@@ -2,70 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public class Landscape : MonoBehaviour {
 
-	[SerializeField]
-	private GameObject grass;
+    [SerializeField]
+    private GameObject finalObject;
 
-	[SerializeField]
-	private GameObject tree;
+    static int[,,] field = new int[9, 9, 2];
+    private Vector2 pos;
+    private int id;
+    private int maxX;
+    private int maxY;
+    // Use this for initialization
+    void Start() {
+        maxX = field.GetLength(0);
+        maxY = field.GetLength(1);
+        for (int i = 0; i < maxX; i++) {
+            field[i, 0, 0] = 1;
+            field[i, 0, 1] = 1;
+            field[i, maxY-1, 0] = 1;
+            field[i, maxY-1, 1] = 1;
+        }
+        for (int j = 0; j < maxY; j++) {
+            field[j, 0, 0] = 1;
+            field[j, 0, 1] = 1;
+            field[j, maxX-1, 0] = 1;
+            field[j, maxX-1, 1] = 1;
+        }
+        for (int k = 0; k < maxX; k++) {
+            for(int l = 0; l < maxY; l++) {
+                pos.x = k;
+                pos.y = l;
+                id = field[k, l, 1];
+                switch (id) {
+                    case 0:
+                        Instantiate(grass, pos, Quaternion.identity);
+                        break;
+                    case 1:
+                        Instantiate(tree, pos, Quaternion.identity);
+                        break;
+                    case 2:
+                        Instantiate(house, pos, Quaternion.identity);
+                        break;
+                }
+            }
+        }
+    }
 
-	[SerializeField]
-	private GameObject house;
-
-	int[,,] field = new int[9, 9, 2];
-	private Vector2 pos;
-
-	// Use this for initialization
-	void Start() {
-		int maxX = field.GetLength(0);
-		int maxY = field.GetLength(1);
-		for (int i = 0; i < maxX; i++)
-		{
-			field[i, 0, 0] = 1;
-			field[i, 0, 1] = 1;
-			field[i, maxY, 0] = 1;
-			field[i, maxY, 1] = 1;
-		}
-		for (int j = 0; j < maxY; j++) {
-			field[j, 0, 0] = 1;
-			field[j, 0, 1] = 1;
-			field[j, maxX, 0] = 1;
-			field[j, maxX, 1] = 1;
-		}
-
-		for (int i = 0; i < field.GetLength(0); i++) {
-			for (int j = 0; j < field.GetLength (1); j++) {
-				pos.x = i;
-				pos.y = j;
-
-				switch(field[i,j,0]){
-
-				case 0: 
-					Instantiate (grass, pos, Quaternion.identity);
-					break;
-
-				case 1: 
-					Instantiate (tree, pos, Quaternion.identity);
-					break;
-				
-				case 2:
-					Instantiate (house, pos, Quaternion.identity);
-					break;
-
-				default:
-					break;
-				}
-			}
-		}
-	}
-
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetMouseButtonDown (0)) {
-			Instantiate (grass, Setup.mousePos, Quaternion.identity);
-		}
+    // Update is called once per frame
+    void Update () {
+		
 	}
 }
