67520445826a637cf3addcdc796293882b0a50e0
RobertKuttig <robert.kuttig@rwth-aachen.de>
2017-10-05 17:48:44 +0200

    Add files via upload

--- Assets/Script/World.cs
+++ Assets/Script/World.cs
@@ -1,41 +1,25 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class World : MonoBehaviour
-{
-    [SerializeField]
-    private GameObject GrassBlock;
+public class NewBehaviourScript : MonoBehaviour {
 
-    int[,,] field = new int[24, 12, 2];
+	[SerializeField]
+	private GameObject finalObject;
 
-    Vector2 position;
-    // Use this for initialization
-    // Generiert eine 24x12 Welt
-    void Start()
-    {
-        /*  z = 0: Block Id
-         *  z = 1: Kollision. Definition unten
-            0 = bauen & laufen
-            1 = bebaut
-            2 = nicht bauen & laufen */
- 
-        for (int i = 0; i < field.GetLength(0); i++)
-        {
-            for (int j = 0; j < field.GetLength(1); j++)
-            {
-                position.x = i;
-                position.y = j;
-                field[i, j, 0] = 1;
-                field[i, j, 1] = 1;
-                //setze Gras Objekt an Koordinate (x,y) in Unity
-                Instantiate(GrassBlock, position, Quaternion.identity); 
-            }
-        }
-    }
+	int[, ,] field = new int[24, 12, 2];
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < field.GetLength(0); i++) {
+			for (int j = 0; j < field.GetLength (1); j++) {
 
-    }
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
