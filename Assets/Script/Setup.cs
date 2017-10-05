7a3056d8e1fec32bead683d7964f3c7b84d5bdeb
RobertKuttig <robert.kuttig@rwth-aachen.de>
2017-10-05 17:40:43 +0200

    Add files via upload

--- Assets/Script/Setup.cs
+++ Assets/Script/Setup.cs
@@ -1,19 +1,32 @@
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Setup : MonoBehaviour {
 
+	public Text mPosition;
+	static public Vector2 mousePos;
+	private Vector2 mousePoint;
+
+
 	// Use this for initialization
 	void Start () {
-		
+		SetMousePos ();
 	}
-
-	public Vector2 mousePoint;
-
+		
 
 	// Update is called once per frame
 	void Update () {
-        mousePoint.x = Camera.main.ScreenToWorldPoint();
+		mousePoint = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		mousePos.x = Mathf.Round (mousePoint.x);
+		mousePos.y = Mathf.Round (mousePoint.y);
+		if (Input.GetMouseButtonDown(0)){
+			SetMousePos ();
+		}
+	}
+
+	void SetMousePos(){
+		mPosition.text = "X-Achse: " + mousePos.x.ToString () + "Y-Achse: " + mousePos.y.ToString ();
 	}
 }
